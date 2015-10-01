019ad4ce5312e764132df506f6efc0e042e41ebd
Luke Griffith <luketgriffith@gmail.com>
2015-10-01 15:54:17 -0400

    finished with #2

--- main.js
+++ main.js
@@ -30,15 +30,39 @@ The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsme
 
 Hand Painted Colorful Feather Glass
 */
+var A =[]  //empty array to push the correct items to
+var priceRange = items.forEach(function(a){      //forEach that finds items with prices between 14 and 18
+  if(a.price > 14 && a.price < 18){
+    A.push(a.title);                            //pushed titles to new array A
+    }
+})
+var item1= A[0];                              //setting variables for the items
+var item2= A[1];
+var item3 =A[2];
+var answer2a = document.querySelector('#answer2a');
+var answer2b = document.querySelector('#answer2b');
+var answer2c = document.querySelector('#answer2c');
+var textNode2 = document.createTextNode(item1);  
+var textNode3= document.createTextNode(item2);
+var textNode4= document.createTextNode(item3);
+answer2a.appendChild(textNode2);
+answer2b.appendChild(textNode3);
+answer2c.appendChild(textNode4);
 
-var prices = items.map( function(item){    //map the price of each object in the array to a new array
-  return item.price;
-});
 
+// var newPrices = items.map( function(item, title){    //map the price of each object in the array to a new array
+//   return item.price, item.title;
+// });
+// var priceRange = prices.filter(function(a){
+//   return a > 14 && a < 18;
 
+// })
+// console.log(priceRange);
+// var title= priceRange.forEach(function(a){
+//   return items.title;
+// })
 
-
-
+// console.log(title);
 
 
 
